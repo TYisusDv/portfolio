b22ef15b01416e64305da94847a7086d5bc88d0b
tyisusdv <jesusns1902@outlook.com>
2024-08-08 13:53:54 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import { Link, Element, scroller } from 'react-scroll';
-import { Home12Icon, UserCircle02Icon, Linkedin01Icon, Mail01Icon, Github01Icon, Facebook01Icon, WhatsappIcon, SkullIcon } from "hugeicons-react";
+import { UserCircle02Icon, Linkedin01Icon, Mail01Icon, Github01Icon, Facebook01Icon, WhatsappIcon, SkullIcon } from "hugeicons-react";
 import { useState, useEffect } from 'react';
 import SkillBar from './assets/components/SkillBar';
 
 function App() {
   const [currentSection, setCurrentSection] = useState('home');
-  const sectionNames = ['home', 'about', 'skills'];
+  const sectionNames = ['about', 'skills'];
+  const [isScrolling, setIsScrolling] = useState(false);
 
   useEffect(() => {
     const handleScroll = (event) => {
+      if (isScrolling) return; // Si está en medio de un scroll, no hacer nada
+
+      const currentIndex = sectionNames.indexOf(currentSection);
+      let nextIndex = currentIndex;
+
       if (event.deltaY > 0) {
-        // Scrolling down
-        const currentIndex = sectionNames.indexOf(currentSection);
-        const nextIndex = Math.min(currentIndex + 1, sectionNames.length - 1);
-        if (currentIndex !== nextIndex) {
-          setCurrentSection(sectionNames[nextIndex]);
-          scroller.scrollTo(sectionNames[nextIndex], {
-            duration: 500,
-            smooth: 'easeInOutQuad',
-          });
-        }
+        nextIndex = Math.min(currentIndex + 1, sectionNames.length - 1);
       } else {
-        // Scrolling up
-        const currentIndex = sectionNames.indexOf(currentSection);
-        const prevIndex = Math.max(currentIndex - 1, 0);
-        if (currentIndex !== prevIndex) {
-          setCurrentSection(sectionNames[prevIndex]);
-          scroller.scrollTo(sectionNames[prevIndex], {
-            duration: 500,
-            smooth: 'easeInOutQuad',
-          });
-        }
+        nextIndex = Math.max(currentIndex - 1, 0);
+      }
+
+      if (currentIndex !== nextIndex) {
+        setIsScrolling(true);
+        setCurrentSection(sectionNames[nextIndex]);
+
+        scroller.scrollTo(sectionNames[nextIndex], {
+          duration: 500,
+          smooth: 'easeInOutQuad',
+        });
+
+        setTimeout(() => {
+          setIsScrolling(false);
+        }, 500);
       }
     };
 
     window.addEventListener('wheel', handleScroll);
     return () => window.removeEventListener('wheel', handleScroll);
-  }, [currentSection]);
+  }, [currentSection, isScrolling]);
 
   const skills_frontend = [
     { skill: 'HTML5', percentage: 100, color: '#E5532D' },
@@ -81,11 +83,6 @@ function App() {
         <div className="body">
           <ul>
             <li>
-              <Link activeClass="active" to="home" spy={true} smooth={true} duration={500}>
-                <Home12Icon />
-              </Link>
-            </li>
-            <li>
               <Link activeClass="active" to="about" spy={true} smooth={true} duration={500}>
                 <UserCircle02Icon />
               </Link>
@@ -99,24 +96,12 @@ function App() {
         </div>
       </nav>
       <div className='container'>
-        <Element name="home" className="element">
-          <section className='page'>
-            <div className='header'>
-              <span className='path'><span className='circle'>⬤</span> Software developer</span>
-              <h1 className='title'>I'm Jesus</h1>
-              <span className='info'>Find out about me and my passion for development.</span>
-            </div>
-            <div className='body'>
-                          
-            </div>
-          </section>
-        </Element>
         <Element name="about" className="element">
           <section className='page'>
             <div className='header'>
-              <span className='path'><span className='circle'>⬤</span> About me</span>
-              <h1 className='title'>Me</h1>
-              <span className='info'>That's how it is! This is me.</span>
+              <span className='path'><span className='circle'>⬤</span> Software developer</span>
+              <h1 className='title'>Hi,</h1>
+              <span className='info'>Discover my passion for software development.</span>
             </div>
             <div className='body'>
               <div className='d-flex justify-content-center'>
@@ -137,7 +122,7 @@ function App() {
                     <div className='description'>
                       <h1>I'm Jesus</h1>
                       <h2 className='text-animated'>Full Stack developer</h2>
-                      <p className='text color-grey overflow-x'>
+                      <p className='text color-grey overflow'>
                         My name is <b>Jesús Navarro Salcido</b>, Computer Systems Engineering student, I am in the <b>9th semester</b> (without starting). I am a <b>Full Stack developer</b> with experience in various <b>technologies</b> and <b>programming languages</b>. My main competencies include: Python, Django, Flask, React, APIs Rest, SQL & NOSQL. I consider myself a versatile professional and always willing to <b>learn new technologies and methodologies</b> to improve my skills and adapt to the needs of the project. I do not limit myself to a single technology, but I continually seek to expand my <b>knowledge</b> to offer the best possible <b>solutions</b>. My approach focuses on <b>problem solving</b>, teamwork and effective <b>communication</b>, ensuring that each project I participate in meets the highest standards of <b>quality and efficiency</b>.
                       </p>
                     </div>
